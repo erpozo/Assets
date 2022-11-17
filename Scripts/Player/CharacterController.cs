4400cafce327af5e03b46f79a45c48ac449c0726
nezorap <nelsonpozoquintana@github.com>
2022-11-17 18:31:16 +0100

    SaltoCorregido

---
+++ Scripts/Player/CharacterController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float jumpForce = 250f;
+    public float rayLenght;
+    private Rigidbody2D rb2D;
+    public LayerMask floor;
+
+    private void Awake(){
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Start(){
+        
+    }
+    // Update is called once per frame
+
+    void Update()
+    {
+        Debug.DrawRay(transform.position, Vector2.down * rayLenght, Color.red);
+
+        if (IsGrounded()){
+            Jump(jumpForce);
+        }
+        Movement();
+    }
+
+    public void Movement()
+    {
+        float right = 10f;
+        float left = -10f;
+
+        if (Input.GetKeyDown(KeyCode.A)){
+            transform.Translate(left* Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D)){
+            transform.Translate(right * Time.deltaTime, 0, 0);
+        }
+    }
+
+    public void Jump(float force)
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)){
+            rb2D.AddForce(Vector2.up * force);
+        }
+    }
+
+    public bool IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, rayLenght, floor);
+        if (hit.collider != null)
+        {
+            Debug.Log("Has colisionado con el suelo");
+            return true;
+        }
+        return false;
+    }
+}
+
+   
