dd1df8ddc1d5d12d9de926d090c7a69a63e1a94d
erpozo <pozo.erp.a@gmail.com>
2022-11-17 12:29:48 +0100

    Initial commit

---
+++ personaje.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class personaje : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w")){
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0,2000f * Time.deltaTime));
+        }
+
+        if (Input.GetKey("a")){
+            gameObject.transform.Translate(-20F * Time.deltaTime,0,0);
+        }
+        
+        if (Input.GetKey("d")){
+            gameObject.transform.Translate(20F * Time.deltaTime,0,0);
+        }
+    }
+}
