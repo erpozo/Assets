8b19172037987521c6cc6bb6c273eabd58438361
erpozo <pozo.erp.a@gmail.com>
2022-11-17 17:26:01 +0100

    Intento Logica salto

--- Scripts/Player/personaje.cs
+++ Scripts/Player/personaje.cs
@@ -5,58 +5,55 @@ using UnityEngine;
 public class personaje : MonoBehaviour
 {
     public float jumpForce = 10;
-    public bool isGrounded = false;
     public Vector2 rayLenght;
-    public Rigidbody2D rb2D;
+    private Rigidbody2D rb2D;
 
-    private void Awake()
-    {
+    private void Awake(){
         rb2D = GetComponent<Rigidbody2D>();
     }
-    void Start()
-    {
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, rayLenght);
+    }
+
+    void Start(){
         
     }
     // Update is called once per frame
-    /**
-    TU codigo es puta mierda*/
+
     void Update()
     {
-        if (isGrounded)
-        {
-            Jump();
+        if (isGrounded()){
+            Jump(jumpForce);
         }
-        
-
-       
+        Movement();
     }
 
     public void Movement()
     {
 
-        if (Input.GetKey("a"))
-        {
+        if (Input.GetKey("a")){
             gameObject.transform.Translate(-20F * Time.deltaTime, 0, 0);
         }
 
-        if (Input.GetKey("d"))
-        {
+        if (Input.GetKey("d")){
             gameObject.transform.Translate(20F * Time.deltaTime, 0, 0);
         }
     }
 
-    public void Jump()
+    public void Jump(float force)
     {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            rb2D.AddForce(Vector2.up * jumpForce);
+        if (Input.GetKeyDown(KeyCode.W)){
+            rb2D.AddForce(Vector2.up * force);
         }
     }
 
-    public void isGrounded()
+    public bool isGrounded()
     {
         RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), rayLenght);
-        Ray ray =   
+        return !hit;
     }
+}
 
    
